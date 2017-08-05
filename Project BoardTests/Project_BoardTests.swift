73c31e5a8b586cf36c6d7ba289134e37a9f97a4e
Joe Soap <joesoap@Joes-Mac.local>
2017-08-05 15:43:22 +0200

    Initial Commit

---
+++ Project BoardTests/Project_BoardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_BoardTests.swift
+//  Project BoardTests
+//
+//  Created by Joe Soap on 2017/08/05.
+//  Copyright © 2017 SinBros. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Board
+
+class Project_BoardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
